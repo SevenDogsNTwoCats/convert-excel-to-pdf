5e985a0492fa1c59cf53b67ce3a84e81f80cdc04
Alhanis Espinal <alhanis.espinal@ginih.com>
2025-08-25 14:21:50 -0600

    refactor: update convertExcelToPdf function to use object destructuring for parameters

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -11,16 +11,25 @@ declare module "excel-to-pdf-converter" {
    * @param {boolean} useMinLimit Whether to enforce minimum size limits (default: false)
    * @param {number} fixedAt Number of decimal places for numeric formatting (default: 2)
    */
-  export function convertExcelToPdf(
-    inputFilePath: string,
-    outputFilePath: string,
-    enablePagination?: boolean,
-    MAX_WIDTH_SIZE?: number,
-    MAX_HEIGHT_SIZE?: number,
-    MIN_WIDTH_SIZE?: number,
-    MIN_HEIGHT_SIZE?: number,
-    useMinLimit?: boolean,
-    fixedAt?: number
-  ): Promise<void>;
-
+  export function convertExcelToPdf({
+    inputFilePath,
+    outputFilePath,
+    enablePagination,
+    MAX_WIDTH_SIZE,
+    MAX_HEIGHT_SIZE,
+    MIN_WIDTH_SIZE,
+    MIN_HEIGHT_SIZE,
+    useMinLimit,
+    fixedAt,
+  }: {
+    inputFilePath: string;
+    outputFilePath: string;
+    enablePagination?: boolean;
+    MAX_WIDTH_SIZE?: number;
+    MAX_HEIGHT_SIZE?: number;
+    MIN_WIDTH_SIZE?: number;
+    MIN_HEIGHT_SIZE?: number;
+    useMinLimit?: boolean;
+    fixedAt?: number;
+  }): Promise<void>;
 }
